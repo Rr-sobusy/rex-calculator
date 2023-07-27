19a42fd840573e87b28375d405b9862286206601
unknown <you@example.com>
2023-07-27 20:43:23 +0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ function App() {
       <div className="__main-container grid justify-center items-center">
         <div className="h-[500px] w-[320px]">
           <div className="grid h-full">
-            <div className="bg-[#243441] rounded-t-[15px]">randy</div>
+            <div className="bg-[#243441] rounded-t-[15px]">
+              <div className="bg-blue-300 mx-7 my-7 h-full rounded-lg">rex</div>
+            </div>
             <Calculator />
           </div>
         </div>
+        <div className="absolute bottom-5 right-10 font-fontPoppins">Created by <span className="font-sans underline">Rex Hernandez</span> {`wid lab <3.`}</div>
       </div>
     </>
   );
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,29 +1,57 @@
 import React from "react";
-import {FaDivide} from 'react-icons/fa'
-
+import { FaDivide, FaEquals, FaAsterisk } from "react-icons/fa";
+import { FiMinus } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 type Props = {};
 
 const Calculator: React.FC<Props> = () => {
   return (
     <>
       <div className="__calc-mainWrapper">
-        {["AC", "_", "_", <FaDivide />].map((val, key) => (
-          <button key={key} className={`bg-[#243441] text-orange-500 active:scale-110 duration-200 overLine customShadow rounded-lg shadow-2xl grid justify-center cursor-pointer items-center font-sans font-semibold`}>
+        {["AC", "_", "_", <FaDivide size={15} />].map((val, key) => (
+          <button
+            key={key}
+            className={`bg-[#243441] text-orange-500 active:scale-110 duration-200 overLine customShadow rounded-lg shadow-2xl grid justify-center cursor-pointer items-center font-sans font-semibold`}
+          >
             {val}
           </button>
         ))}
-        {[7, 8, 9, "_"].map((val, key) => (
-          <div key={key} className="bg-[#243441] text-slate-400 overLine customShadow grid justify-center cursor-pointer items-center rounded-lg shadow-lg">{val}</div>
+        {[7, 8, 9, <FaAsterisk size={14} color="#F97316" />].map((val, key) => (
+          <button
+            key={key}
+            className={`bg-[#243441]  text-slate-400 active:scale-110 duration-200 overLine customShadow grid justify-center cursor-pointer items-center rounded-lg shadow-lg`}
+          >
+            {val}
+          </button>
         ))}
-        {[4, 5, 6, "_"].map((val, key) => (
-          <div key={key} className="bg-[#243441]  customShadow overLine grid justify-center cursor-pointer items-center rounded-lg shadow-lg">{val}</div>
+        {[4, 5, 6, <FiMinus size={18} color="#F97316" />].map((val, key) => (
+          <button
+            key={key}
+            className="bg-[#243441] text-slate-400  customShadow active:scale-110 duration-200 overLine grid justify-center cursor-pointer items-center rounded-lg shadow-lg"
+          >
+            {val}
+          </button>
         ))}
-        {[1, 2, 3, "_"].map((val, key) => (
-          <div key={key} className="bg-[#243441] customShadow overLine grid justify-center cursor-pointer items-center rounded-lg shadow-lg">{val}</div>
+        {[1, 2, 3, <AiOutlinePlus size={20} color="#F97316" />].map(
+          (val, key) => (
+            <button
+              key={key}
+              className="bg-[#243441] text-slate-400 customShadow active:scale-110 duration-200 overLine grid justify-center cursor-pointer items-center rounded-lg shadow-lg"
+            >
+              {val}
+            </button>
+          )
+        )}
+        {[0, ".", <FaEquals color="#F97316" />].map((val, key) => (
+          <button
+            key={key}
+            className={`grid justify-center ${
+              key === 2 ? "col-span-2 text-orange-500" : null
+            } overLine items-center text-slate-400 bg-[#243441] customShadow rounded-lg`}
+          >
+            {val}
+          </button>
         ))}
-        <div className="grid justify-center overLine items-center text-slate-400 bg-[#243441] customShadow rounded-lg">0</div>
-        <div className="grid justify-center overLine items-center text-slate-400 bg-[#243441] customShadow rounded-lg">.</div>
-        <div className="col-span-2 bg-blue-100">rex</div>
       </div>
     </>
   );
